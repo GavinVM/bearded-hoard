7ebf35a23d5cef231eadce59b99052b36df2c83a
Gavin <gavin.v.mitchell@hotmail.co.uk>
2023-11-13 21:07:38 +0000

    adding some enviroment elements

---
+++ src/app/model/entry.model.ts
@@ -0,0 +1,8 @@
+export interface IEntry {
+    title: string,
+    overview: string,
+    image: string
+    apiGenreIds: number[],
+    apiId: number,
+    kind: string
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  baseTMDBUrl: 'https://api.themoviedb.org/3/',
+  movieSearchUrl: 'search/movie',
+  tvSearchUrl: 'search/tv',
+  multiSearchUrl: 'search/multi',
+  accessTokenAuth:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzRmNDA5YmJjMTExNTY3Yjk5YjYxZmQxMmZkMzFkZSIsInN1YiI6IjYzNGRjZWMyZWY5ZDcyMDA5MTY0ZGEwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5YVA4TnXy27q2v70iqdJUCE5WFupBXXiwc8jEzLYkNs',
+  apiKeyAuth: 'd74f409bbc111567b99b61fd12fd31de',
+};
