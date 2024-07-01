680fa6ea4f363d7f34caedbc8dad182a6956f445
Gavin Mitchell <duke_lionstt@hotmail.com>
2024-07-01 16:26:32 +0100

    added reorder functionality, including persisting

--- src/app/tracker/tracker.page.ts
+++ src/app/tracker/tracker.page.ts
@@ -4,6 +4,11 @@ import { Entry } from '../model/entry.model';
 import { StorageResponse } from '../model/storage-response.model';
 import { TabsService } from '../service/tabs.service';
 import { environment } from 'src/environments/environment';
+import { ItemReorderEventDetail } from '@ionic/angular';
+
+const GRID: string = 'grid';
+const REORDER_TOGGLED: string = 'filled';
+const REORDER_PENDING: string = '';
 
 @Component({
   selector: 'app-tracker',
@@ -16,8 +21,12 @@ export class TrackerPage implements OnInit{
   isWaitingForStorage!: boolean;
   isGrid!: boolean;
   isReorder!: boolean;
+  changeReorder!: boolean;
+  savePopoverState!: boolean;
   trackerList!: Entry[];
+  previousTrackerList!: Entry[];
   imagePreFix!: string;
+  reorderButtonState! :string;
 
   constructor(private appDataService: AppDataService,
               private tabService: TabsService) {}
@@ -28,6 +37,10 @@ export class TrackerPage implements OnInit{
     this.isWaitingForStorage = false;
     this.isGrid = true;
     this.isReorder = false;
+    this.changeReorder = false;
+    this.savePopoverState = false;
+    this.reorderButtonState = REORDER_PENDING;
+    this.previousTrackerList = []
     this.imagePreFix = environment.tmdbImageBase;
     this.tabService.tabChangingEmiter.subscribe(tab => this.activeTabReload(tab))
     this.loadTrackerList();
@@ -75,9 +88,15 @@ export class TrackerPage implements OnInit{
     })
   }
 
-  toggleGridListView(): boolean {
-    console.info(`mrTracker.TrackerPage.toggleGridListView:: flipping isGrid`)
-    return this.isGrid = !this.isGrid;
+  toggleGridListView(event: any): void {
+    console.info(`mrTracker.TrackerPage.toggleGridListView:: starting`)
+    console.debug(`mrTracker.TrackerPage.toggleGridListView:: segemtn triggered passing in ->`, event)
+    if(event.detail.value.includes(GRID)){
+      this.isGrid = true;
+    } else {
+      this.isGrid = false;
+    }
+    console.info(`mrTracker.TrackerPage.toggleGridListView:: finishing`)
   }
 
   formatDetail(entry: Entry): string{
@@ -85,6 +104,45 @@ export class TrackerPage implements OnInit{
     return entry.overview.substring(0, entry.overview.indexOf(' ', detailLength))
   }
 
-  
+  toggleReorder(): void{
+    console.info(`mrTracker.TrackerPage.toggleReorder:: starting`)
+    if(!this.isReorder){
+      console.info(`mrTracker.TrackerPage.toggleReorder:: setting to true`)
+      this.previousTrackerList = this.trackerList
+      this.isReorder = true;
+      this.reorderButtonState = REORDER_TOGGLED
+      this.savePopoverState = true;
+      console.info(`mrTracker.TrackerPage.toggleReorder:: ready to accept changes`)
+    } else if(this.isReorder) {
+      console.info(`mrTracker.TrackerPage.toggleReorder:: setting to false`)
+      if(this.changeReorder){
+        console.info(`mrTracker.TrackerPage.toggleReorder:: saving changes`)
+        this.appDataService.updateTrackerList(this.previousTrackerList)
+      .then((response: StorageResponse) => {
+        if(response.status){
+          
+        } else {
+          console.info(`mrTracker.TrackerPage.toggleReorder:: issue saving new order: \n\t\t\t\t${response.errorMessage}`)
+          console.info(`mrTracker.TrackerPage.toggleReorder:: reverting changes`)
+          this.trackerList = this.previousTrackerList
+        }
+        
+      })
+      } 
+      console.info(`mrTracker.TrackerPage.toggleReorder:: reverting state to statndard list`)
+      this.reorderButtonState = REORDER_PENDING
+      this.isReorder = false;
+    }
+    console.info(`mrTracker.TrackerPage.toggleReorder:: finishing`)
+  }
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>){
+    console.info(`mrTracker.TrackerPage.handleReorder:: starting`)
+    if(!this.changeReorder) this.changeReorder = true;
+    this.trackerList = ev.detail.complete(this.trackerList)
+    console.info(`mrTracker.TrackerPage.handleReorder:: finishing`)
+    // this.trackerList = ev.detail.complete(this.trackerList)
+    
+  }
 
 }
