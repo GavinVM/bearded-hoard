7c5e84595713aa26b35ec002877ee43df34494a2
Gavin Mitchell <duke_lionstt@hotmail.com>
2024-07-09 00:38:00 +0100

    displays tv keyword searchs as each season and as a box set

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppDataService } from '../service/app-data.service';
 import { StorageService } from '../service/storage.service';
-import { switchMap } from 'rxjs';
+import { forkJoin, map, mergeMap, of, switchMap } from 'rxjs';
 import { Entry } from '../model/entry.model';
 import { StorageResponse } from '../model/storage-response.model';
 import { TabsService } from '../service/tabs.service';
@@ -19,9 +19,11 @@ export class AddPage implements OnInit{
   toastMessage!: string;
   currentFormat!: string;
   isToast!: boolean;
+  isSeasonDetailsRunning!: boolean;
 
   results!: any[];
   options!: any[];
+  seasonsDetails!: any
 
   isSavedBluray!: Map<string, boolean>;
   isSaved4k!: Map<string, boolean>;
@@ -37,11 +39,13 @@ export class AddPage implements OnInit{
   ngOnInit(): void {
     this.results = [];
     this.options = [];
+    this.seasonsDetails =[];
     this.isSavedBluray = new Map();
     this.isSaved4k = new Map();
     this.isLoadingBluray = new Map();
     this.isLoading4k = new Map();
     this.isToast = false;
+    this.isSeasonDetailsRunning = false;
     this.appDataService.savedEventEmittter.subscribe(response => this.handleSavingEvent(response));
     this.tabService.tabChangingEmiter.subscribe(tab => this.tabChange(tab));
   }
@@ -55,39 +59,73 @@ export class AddPage implements OnInit{
   
   getResults(event:any){
     const val = event.target.value;
+    this.options = [];
+    this.isSavedBluray = new Map();
+    this.isSaved4k = new Map();
+    this.isLoadingBluray = new Map();
+    this.isLoading4k = new Map();
     if (val != '') {
-      this.appDataService.getSearchResults(val).subscribe({
-        next: (data: any) => {
-          console.debug(data);
-          this.results = data.results;
-          this.isSavedBluray = new Map();
-          this.isSaved4k = new Map();
-          this.isLoadingBluray = new Map();
-          this.isLoading4k = new Map();
-          this.options = data.results.filter((result:any) => result.media_type != 'person').map((result: any) => {
-            this.isSavedBluray.set(result.id, false);
-            this.isSaved4k.set(result.id, false);
-            this.isLoadingBluray.set(result.id, false);
-            this.isLoading4k.set(result.id, false);
-            return {
-              title: result.media_type == 'tv'? result.name : result.title,
-              id: result.id,
-              mediaType: result.media_type,
-              releaseYear: new Date(result.media_type == 'tv'? result.first_air_date : result.release_date).getFullYear().toString()
-            };
-          });
-          console.debug(`MrTracker.AddEntriesComponent.ngOnInit - next line is options`)
-          console.debug(this.options)
-        },
-        error: (error) => {
-          console.error('oops');
-          console.error(error);
-          this.options = [];
-        },
-      });
-    } else {
-      this.options = [];
+      this.appDataService.getSearchResults(val).pipe(
+          mergeMap((api:any) => {
+            return api.results.filter((result:any) => result.media_type != 'person').map((result:any) => {
+                return this.appDataService.geTvDetailsById(result.id, result.media_type, result).pipe(map((result:any) => {  return result}))
+              })
+            } 
+          )
+        ).subscribe({
+          next: (data: any) => {
+            console.debug(data);
+            this.results = data;
+            data.subscribe((result: any) => {
+              this.isSavedBluray.set(result.id, false);
+              this.isSaved4k.set(result.id, false);
+              this.isLoadingBluray.set(result.id, false);
+              this.isLoading4k.set(result.id, false);
+              if(result.media_type != 'movie') {
+                this.formatResultsForTvSeasons(result).forEach((season:any) => {
+                  this.options.push(season);
+                })
+              } else {
+                this.options.push({
+                  title: result.title,
+                  id: result.id,
+                  mediaType: result.media_type,
+                  releaseYear: new Date(result.release_date).getFullYear().toString()
+                })
+              }
+            });
+            console.debug(`MrTracker.AddEntriesComponent.ngOnInit - next line is options`)
+            console.debug(this.options)
+          },
+          error: (error) => {
+            console.error('oops');
+            console.error(error);
+            this.options = [];
+          } 
+        })
+        
     }
+  }
+
+  formatResultsForTvSeasons(result:any){
+    console.log("`mrTracker.AddPage.saveEntry:: result", result)
+    let tempOptions: any[] = [];
+    result.seasons.forEach((season:any) => {
+      tempOptions.push({
+        title: `${result.name}: ${season.name}`,
+        id: result.id,
+        mediaType: result.media_type,
+        releaseYear: new Date(season.air_date).getFullYear().toString()
+      })
+    });
+
+    tempOptions.push({
+      title: `${result.name}: Box Set`,
+      id: result.id,
+      mediaType: result.media_type,
+      releaseYear: new Date(result.first_air_date).getFullYear().toString()
+    }) 
+    return tempOptions;
   }
 
   handleSelection( selection:any, saveState: boolean, format: string){
